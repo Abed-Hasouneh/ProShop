26361616112049af881cd255626788251ade843d
Abed-Hasouneh <a.r.hasouneh@gmail.com>
2022-03-03 22:16:24 +0200

    Routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,29 @@
 import React from 'react';
+import { BrowserRouter,Routes,Route } from "react-router-dom";
+
 import './App.css';
+
 import Header from './components/Header';
 import Footer from './components/Footer';
 
 import {Container} from 'react-bootstrap'
 import HomeScreen from './pages/HomeScreen';
+import ProductScreen from './pages/ProductScreen';
 
 function App() {
   return (
-    <>
+    <BrowserRouter>
       <Header/>
         <main className='py-3'>
           <Container>
-            <HomeScreen/>
+          <Routes>
+            <Route path="/" element={<HomeScreen />} />
+            <Route path="product/:id" element={<ProductScreen />} />
+          </Routes>
           </Container>
         </main>
       <Footer/>
-    </>
+    </BrowserRouter>
   );
 }
 
